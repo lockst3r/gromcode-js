67f8aa16d975d0348301f8aec0557dbce87fad4b
lockst3r <ushakovqq@gmail.com>
2020-12-10 19:07:40 +0200

    ll

---
+++ lesson9/balances/index.js
@@ -0,0 +1,9 @@
+'use strick';
+
+//input arr
+//output num
+
+function getTotalRevenue(arr) {
+  return arr.map(el => Object.values(el)[1]).reduce((acc, n) => acc + n);
+}
+
--- lesson9/balances/script.js
+++
@@ -1,7 +0,0 @@
-const getTotalRevenue = (dayTransactions) => {
-  const result = dayTransactions.reduce(function (acc, el) {
-    return acc + el.amount;
-  }, 0);
-
-  return result;
-};
