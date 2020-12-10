d00383726ac56b67d7d9e87ca31daca5bb76c37e
lockst3r <ushakovqq@gmail.com>
2020-12-10 18:56:52 +0200

    gg

--- lesson9/balances/script.js
+++ lesson9/balances/script.js
@@ -1,11 +1,18 @@
 'use strick';
 
-//input arr
-//output num
+const dayTransactions = [
+  { userId: 22, amount: 60, operation: 'sell' },
+  { userId: 22, amount: 160, operation: 'buy' },
+  { userId: 44, amount: 90, operation: 'sell' },
+];
 
-function getTotalRevenue(arr) {
+
+
+function getTotalRevenue (arr) {
   return arr.map(el => Object.values(el)[1]).reduce((acc, n) => acc + n);
 }
 
+
+console.log(getTotalRevenue(dayTransactions));
 
 
