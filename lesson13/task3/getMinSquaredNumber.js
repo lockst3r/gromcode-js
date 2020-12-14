bd394e95cb38bcb4cf3a959022758af464dbcfe9
lockst3r <ushakovqq@gmail.com>
2020-12-14 18:07:11 +0200

    dd

--- lesson13/task3/getMinSquaredNumber.js
+++ lesson13/task3/getMinSquaredNumber.js
@@ -9,7 +9,12 @@
 
 
 export  const getMinSquaredNumber = (arr) => {
+  if(!Array.isArray(arr)||arr.length === 0){
+  return null;
+  // eslint-disable-next-line no-else-return
+  }else{
   const modul = arr.map((el) => Math.abs(el));
   const minNumber = Math.min(...modul);
   return minNumber * minNumber;
+}
 };
