ff49c2ec3550b13cdc0a355367c9550496d4ab54
lockst3r <ushakovqq@gmail.com>
2020-12-22 14:50:26 +0200

    add

---
+++ lesson21/task-6-hw-clean-item/index.js
@@ -0,0 +1,11 @@
+/* eslint-disable strict */
+/* eslint-disable no-unused-vars */
+/* eslint-disable func-names */
+// eslint-disable-next-line lines-around-directive
+
+
+export const clearList =() => {
+const getItemCategories = document.querySelector('.categories');
+getItemCategories.innerHTML = '';
+}
+clearList();
