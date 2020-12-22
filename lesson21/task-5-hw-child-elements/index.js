051ee3ae71c2e929a3ccf2f61393f0b7aac60f38
lockst3r <ushakovqq@gmail.com>
2020-12-22 14:39:36 +0200

    hh

---
+++ lesson21/task-5-hw-child-elements/index.js
@@ -0,0 +1,9 @@
+/* eslint-disable strict */
+/* eslint-disable no-unused-vars */
+/* eslint-disable func-names */
+// eslint-disable-next-line lines-around-directive
+
+const setButton = (buttonText) => {
+  const getItemBody = document.querySelector("body");
+  getItemBody.innerHTML = `<button>${buttonText}</button>`;
+};
