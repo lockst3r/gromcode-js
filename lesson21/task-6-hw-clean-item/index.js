2d7bf690fab5d12d74f7013d815ac4b51ece98d9
lockst3r <ushakovqq@gmail.com>
2020-12-22 14:53:12 +0200

    add

--- lesson21/task-6-hw-clean-item/index.js
+++ lesson21/task-6-hw-clean-item/index.js
@@ -3,9 +3,7 @@
 /* eslint-disable func-names */
 // eslint-disable-next-line lines-around-directive
 
-
-export const clearList =() => {
-const getItemCategories = document.querySelector('.categories');
-getItemCategories.innerHTML = '';
-}
-clearList();
+export const clearList = () => {
+  const categoriesElem = document.querySelector(".categories");
+  categoriesElem.innerHTML = "";
+};
