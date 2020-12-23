54f85a210770a06452ea93238b42f25fc07e4043
lockst3r <ushakovqq@gmail.com>
2020-12-23 16:26:02 +0200

    add

---
+++ lesson22/task-1-hw-click/index.js
@@ -0,0 +1,12 @@
+/* eslint-disable no-alert */
+/* eslint-disable strict */
+/* eslint-disable no-unused-vars */
+/* eslint-disable func-names */
+// eslint-disable-next-line lines-around-directive
+
+const getItemBtn = document.querySelector(".create-btn");
+
+const showAlert = () => {
+  alert("done");
+};
+getItemBtn.addEventListener("click", showAlert);
