e1527354815269bdf7e25f941928411606bfbd94
lockst3r <ushakovqq@gmail.com>
2020-12-29 15:15:38 +0200

    kk

--- lesson29/task1/index.js
+++ lesson29/task1/index.js
@@ -1,7 +1,9 @@
+/* eslint-disable no-unused-vars */
 export const addImage = (imgSrc, callback) => {
   const imgElem = document.createElement("img");
   imgElem.setAttribute("alt", "My photo");
   imgElem.src = imgSrc;
+
   const getItemPage = document.querySelector(".page");
   getItemPage.append(imgElem);
 
@@ -11,20 +13,19 @@ export const addImage = (imgSrc, callback) => {
   };
   imgElem.addEventListener("load", onImageLoaded);
   imgElem.addEventListener("error", () =>
-    callback("Image load is failed...")
+    callback("Image load failed")
   );
 };
 
 const imgSrc =
   "https://p.bigstockphoto.com/GeFvQkBbSLaMdpKXF1Zv_bigstock-Aerial-View-Of-Blue-Lakes-And--227291596.jpg";
 
-const callback = (error, data) => {
+const onImageLoaded = (error, data) => {
   if (error) {
-    console.log(error);
-    return;
+    return error;
   }
   const { width, height } = data;
   const sizeElem = document.querySelector(".image-size");
   sizeElem.textContent = `${width} x ${height}`;
 };
-addImage(imgSrc, callback);
+
