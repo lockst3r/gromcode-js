16a32e7d83da2d4c9573824b2805ff003faf9ab8
lockst3r <ushakovqq@gmail.com>
2020-12-29 15:37:33 +0200

    add

--- lesson29/task1/index.js
+++ lesson29/task1/index.js
@@ -1,3 +1,4 @@
+/* eslint-disable consistent-return */
 /* eslint-disable no-unused-vars */
 export const addImage = (imgSrc, callback) => {
   const imgElem = document.createElement("img");
@@ -23,3 +24,5 @@ const onImageLoaded = (error, data) => {
   const sizeElem = document.querySelector(".image-size");
   sizeElem.textContent = `${width} x ${height}`;
 };
+
+addImage("https://p.bigstockphoto.com/GeFvQkBbSLaMdpKXF1Zv_bigstock-Aerial-View-Of-Blue-Lakes-And--227291596.jpg", onImageLoaded);
